f241c1908a9bb1972f02086e9dcffe5e98d9638f
cyprienlardier <cyprien.lardier@etu.unilim.fr>
2025-10-24 14:04:23 +0200

    Gitpage

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,36 @@
+
+
+
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
+
+
 export default defineConfig({
-  plugins: [tailwindcss()],
-  base: "/SAE-301/",
+ plugins: [tailwindcss()],
+ base: "/SAE-301/",
+ build: {
+   rollupOptions: {
+     input: {
+      main: resolve(__dirname, "index.html"),
+      category: resolve(__dirname, "Catégory.html"),
+      commande: resolve(__dirname, "commande.html"),
+      confirmation: resolve(__dirname, "confirmation.html"),
+      connexion: resolve(__dirname, "connexion.html"),
+      detail: resolve(__dirname, "Detail.html"),
+      detailGris: resolve(__dirname, "Detail-gris.html"),
+      detailRose: resolve(__dirname, "Detail-rose.html"),
+      inscription: resolve(__dirname, "inscription.html"),
+      inviter: resolve(__dirname, "inviter.html"),
+      it1: resolve(__dirname, "It-1.html"),
+      panier: resolve(__dirname, "Panier.html"),
+      produit: resolve(__dirname, "Produit.html"),
+      profil: resolve(__dirname, "Profil.html"),
+      validation: resolve(__dirname, "validation.html"),
+     
+     },
+   },
+ },
 });
+
+
